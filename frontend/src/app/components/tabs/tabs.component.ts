587bbcd729133c7898e66e939775bd3baf5ad570
Fonthy <chryslerfonthy@gmail.com>
2025-04-13 09:24:46 +0300

    HttpClient error

--- frontend/src/app/components/tabs/tabs.component.ts
+++ frontend/src/app/components/tabs/tabs.component.ts
@@ -4,24 +4,31 @@ import { ProductService } from '../../../service/productservice';
 import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-tabs',
   imports: [TableModule, CommonModule, ButtonComponent],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.css',
-  standalone: true,
-  providers: [ProductService,ButtonComponent]
+  standalone: true
 })
-
 export class TabsComponent implements OnInit {
-    products!: Product[];
+  products!: Product[];
 
-    constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+  ) {}
 
-    ngOnInit() {
-        this.productService.getProductsMini().then((data) => {
-            this.products = data;
-        });
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+        next: (data) => {
+          this.products = data;
+          console.log('Utilisateurs récupérés :', this.products);
+        },
+        error: (err) => {
+          console.error('Erreur lors de la récupération des utilisateurs', err);
+        }
+      });
     }
 }
